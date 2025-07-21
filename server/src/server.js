b5cc78b22360c7c914b0f74c6977f3e2217e8408
Khushshaikh74 <146561682+Khushshaikh74@users.noreply.github.com>
2025-07-21 16:42:13 +0530

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(
     cors({
-        origin : "http://localhost:5173",
+        origin : "https://connectify-client-feyb.onrender.com",
         credentials : true
     })
 );
